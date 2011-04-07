7043874176744433d9c549d328818e1e11e91eb6
scott lackey <scottlackey@gmail.com>
2011-04-07 16:25:45 -0700

    mostly changes to Rose::DB test scripts

--- RoseDB/t2.pl
+++ RoseDB/t2.pl
@@ -1,8 +1,22 @@
 #!/usr/bin/perl 
 use strict;
 use warnings;
-use Resource;
+use Provisioner;
+
+#This is a test program to try out loading values into the DB using Rose::DB:Object methods rather than writing SQL. So far this funtionality works, with each table name as it's own object defined in 'Provisioner.pm'
 
 my $p = Resource->new(resource_id => '1');
     print $p->resource_id;
 
+#Save a value to the DB
+#my $u = User->new(username => 'testuserfromRoseDB',
+#		  password => 'test'
+#);
+
+#$u->save;
+
+#Load a value to the DB
+my $u = User->new(user_id => '1');
+$u->load;
+
+	print "\n" . $u->username . "\n";
