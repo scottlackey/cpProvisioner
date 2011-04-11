458f84c80780def34f461f9e93d4983022ca0303
scott lackey <scottlackey@gmail.com>
2011-04-11 16:53:38 -0700

    added DB::Manager objects for all tables defined in Provisioner.pm in Manager.pm

---
+++ RoseDB/My/Manager.pm
@@ -0,0 +1,85 @@
+package Appenv::Manager;
+use base qw(Rose::DB::Object::Manager);
+
+    sub object_class { 'Appenv' }
+
+    __PACKAGE__->make_manager_methods('appenvs');
+package Appenvversion::Manager;
+use base qw(Rose::DB::Object::Manager);
+
+    sub object_class { 'Appenvversion' }
+
+    __PACKAGE__->make_manager_methods('appenvversions');
+package Appinstance::Manager;
+use base qw(Rose::DB::Object::Manager);
+
+    sub object_class { 'Appinstance' }
+
+    __PACKAGE__->make_manager_methods('appinstances');
+package Appservers::Manager;
+use base qw(Rose::DB::Object::Manager);
+
+    sub object_class { 'Appserver' }
+
+    __PACKAGE__->make_manager_methods('appservers');
+package Chrootimage::Manager;
+use base qw(Rose::DB::Object::Manager);
+
+    sub object_class { 'Chrootimage' }
+
+    __PACKAGE__->make_manager_methods('chrootimages');
+package Osimage::Manager;
+use base qw(Rose::DB::Object::Manager);
+
+    sub object_class { 'Osimage' }
+
+    __PACKAGE__->make_manager_methods('osimages');
+package Publicinterface::Manager;
+use base qw(Rose::DB::Object::Manager);
+
+    sub object_class { 'Publicinterface' }
+
+    __PACKAGE__->make_manager_methods('publicinterfaces');
+package Resource::Manager;
+use base qw(Rose::DB::Object::Manager);
+
+    sub object_class { 'Resource' }
+
+    __PACKAGE__->make_manager_methods('resources');
+package Resourcetype::Manager;
+use base qw(Rose::DB::Object::Manager);
+
+    sub object_class { 'Resourcetype' }
+
+    __PACKAGE__->make_manager_methods('resourcetypes');
+package Resourceversion::Manager;
+use base qw(Rose::DB::Object::Manager);
+
+    sub object_class { 'Resourceversion' }
+
+    __PACKAGE__->make_manager_methods('resourceversions');
+package Resourceaccess::Manager;
+use base qw(Rose::DB::Object::Manager);
+
+    sub object_class { 'Resourceaccess' }
+
+    __PACKAGE__->make_manager_methods('resourceaccesses');
+package Resourceserver::Manager;
+use base qw(Rose::DB::Object::Manager);
+
+    sub object_class { 'Resourceserver' }
+
+    __PACKAGE__->make_manager_methods('resourceservers');
+package Threadpack::Manager;
+use base qw(Rose::DB::Object::Manager);
+
+    sub object_class { 'Threadpack' }
+
+    __PACKAGE__->make_manager_methods('threadpacks');
+package User::Manager;
+use base qw(Rose::DB::Object::Manager);
+
+    sub object_class { 'User' }
+
+    __PACKAGE__->make_manager_methods('users');
+1;
--- RoseDB/t2.pl
+++ RoseDB/t2.pl
@@ -1,16 +1,20 @@
 #!/usr/bin/perl 
 use strict;
 use warnings;
-use Provisioner;
-
 #This is a test program to try out loading values into the DB using Rose::DB:Object methods rather than writing SQL. So far this funtionality works, with each table name as it's own object defined in 'Provisioner.pm'
+
+#Our Rose::Db schema map as well as other object definitions.
+use My::Provisioner;
+#Manager objects are Rose::DB::Objects that correspond to methods for operating on more than one item in the DB, which is usually the case and it seems stupid that there is a separate module and set of objects for that, but whatever.
+use My::Manager;
+
 
 my $p = Resourcetype->new(resource_type_name => 'mysql',
 		tcpip_based => '1', 
 		requires_agent => '1',	
 		standard_port => '3306'
 );
- my $res = Resourcetype::Manager->get_resource_types();
+ my $res = Resourcetype::Manager->get_resourcetypes();
  foreach my $r (@$res) {
 	print "\n Resource Type " . $r->resource_type_name . "\n";
 }
@@ -22,10 +26,10 @@ $p->save;
 my $su = User->new(username => 'slackey',
 		  password => 'test'
 );
-$su->save;
+#$su->save;
 
 #Load a value from the DB
-my $lu = User->new(user_id => '2');
+my $lu = User->new(user_id => '1');
 $lu->load;
 
 	print "\n" . $lu->username . "\n";
