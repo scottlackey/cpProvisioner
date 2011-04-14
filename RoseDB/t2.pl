dd811ad452595794253eebd774dd9c92e0425416
scott lackey <scottlackey@gmail.com>
2011-04-13 20:36:10 -0700

    more loader work

--- RoseDB/t2.pl
+++ RoseDB/t2.pl
@@ -8,20 +8,49 @@ use My::Provisioner;
 #Manager objects are Rose::DB::Objects that correspond to methods for operating on more than one item in the DB, which is usually the case and it seems stupid that there is a separate module and set of objects for that, but whatever.
 use My::Manager;
 
+rtype('mysql','1','1','3306');
+rtype('memcache','1','0','11211');
+rtype('storage','1','0','2049');
 
-my $p = Resourcetype->new(resource_type_name => 'mysql',
-		tcpip_based => '1', 
-		requires_agent => '1',	
-		standard_port => '3306'
-);
-    print $p->resource_type_name;
-$p->save;
+my $rtypes = [ 'mysql', 'memcache', 'storage'];
+
+rserver('localhost', '1', $rtypes);
+
+sub rtype{
+	my ($tname, $tcpip, $agent, $port) = @_;
+	my $p = Resourcetype->new(resource_type_name => $tname,
+		tcpip_based => $tcpip, 
+		requires_agent => $agent,	
+		standard_port => $port
+	);
+	$p->save;
+}
+
+sub rserver{
+	my ($host, $os, $rtypes) = @_;
+
+	my $p = Resourceserver->new(hostname => $host,
+		osimage_id => $os
+	);
+	print "Resource Server " . $p->hostname . " created\n";
+	$p->save;
+
+	foreach my $item (@$rtypes){
+		my $r = Resourcetype->new(resource_type_name => $item);
+		$r->load;
+		$p->resource_type_id($r->resource_type_id);
+print "resourcetypeID " . $p->resource_type_id . "  \n";
+		$p->save;
+	}
+}
+
+
 
 #Manager multiple values
- my $res = Resourcetype::Manager->get_resourcetypes();
- foreach my $r (@$res) {
-	print "\n Resource Type " . $r->resource_type_name . $r->resource_type_id . "\n";
-}
+# my $res = Resourcetype::Manager->get_resourcetypes();
+# foreach my $r (@$res) {
+#	print "\n Resource Type " . $r->resource_type_name . $r->resource_type_id . "\n";
+#}
 
 
 #Save a value to the DB
