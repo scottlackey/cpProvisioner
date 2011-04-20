a01ec6d0ce73fd5aa3dd1205dd0b46e5d99bd05a
scott lackey <scottlackey@gmail.com>
2011-04-20 14:19:22 -0700

    first fully-funtional set

--- RoseDB/provision.pl
+++ RoseDB/provision.pl
@@ -21,52 +21,119 @@ die $usage unless ($user);
 
 my $u = User->new(username => $user);
 unless($u->load(speculative => 1)) {
-      die "No such user in DB";
+      die "No such user $user can be found\n";
     }
 
 my $threads = '1';
-#my $ai = createAI($u->user_id);
-#my $tp = createTP($ai, $threads);
 
-#print "APPInstance = $ai, ThreadPack = $tp\n";
+my $as = getAppServer($threads);
+my $ai = createAI($u->user_id);
+addResourceToAI(['mysql', 'memcached'], '1', $user);
+my $tp = createTP($ai, $threads, $as);
 
 #attach AI to TP
-#attachAITP($ai, $tp);
+#my $ai='1';
+#my $tp='1';
+attachAITP($ai, $tp);
 
-getAppServer($threads);
+print "APPInstance = $ai, attached to ThreadPack = $tp for user $user with $threads thread(s) on appserver #$as\n";
 
 #end main
 #subs
 #################################################################################################
-#UNUSED
-sub checkTP{
-my $tc = shift;
-	my $tp = Threadpack->new(thread_count => $tc
-				);
+
+# Attaches a TP to an AI, and makes it live.
+sub attachAITP{
+my ($ai_id, $tp_id) = @_;
+my $tp = Threadpack->new(threadpack_id => $tp_id);
+unless($tp->load(speculative => 1)) {
+      die "No such threadpack ID $tp_id can be found\n";
+    }
+my $ai = Appinstance->new(appinstance_id => $ai_id);
+unless($ai->load(speculative => 1)) {
+      die "No such appinstance ID $ai_id can be found\n";
+    }
+
+$tp->is_live('1');
+$tp->used('1');
+$tp->locked('0');
+$tp->save;
+
+$ai->threads_requested($ai->threads_live + $tp->thread_count);
+$ai->threads_live($ai->threads_live + $tp->thread_count);
+$ai->is_live('1');
+$ai->save;
 }
 
+
+# Finds and locks a give resource_type and returns the resource_id
+sub findRes{
+my ($rtype) = shift;
+my $resource; #resource object to return the ID of.
+my $rt = Resourcetype->new(resource_type_name => $rtype);
+unless($rt->load(speculative => 1)) {
+      die "No such resourcetype $rtype can be found\n";
+    }
+my $res = Resource::Manager->get_resource();
+foreach my $r (@$res) {
+	if ($r->resource_type_id == $rt->resource_type_id && $r->locked == '0' && $r->used == '0') {
+		$resource = Resource->new(resource_id => $r->resource_id);
+		unless($resource->load(speculative => 1)) {
+      			die "No such resource ID " . $r->resource_id . " can be found\n";
+    		}
+		$resource->locked('1');
+		last;
+	}		
+}
+$resource->save;
+return $resource->resource_id;
+}
+
+#adds a list of resourcetypes to an appinstance by updating the ResourceAccess table
+sub addResourceToAI{
+my ($rtypes, $ai, $user) = @_;
+foreach my $each (@$rtypes){ 
+	print " Allocating resourcetype: $each \n"; 
+	my $res = findRes($each);
+	if ($res){
+		my $ra = Resourceaccess->new(appinstance_id => $ai,
+					     authorizing_account_id => $user,
+			     		     resource_id => $res);
+		$ra->save;
+print "appinst ID = $ai, accountID = $user, resource_id = $res\n";
+	}
+	else { die " Could not find available resource type $each \n"; }
+}
+}
+
+#Check for a viable Appserver that has enough threads available, or exit with error
 sub getAppServer{
 my $threads = shift;
-
+my $appserverid;
 my $res = Appserver::Manager->get_appserver();
 foreach my $r (@$res) {
-       print "\n Appserver " . $r->hostname . " Thread Count ". $r->thread_count ."\n";
-	if ($r->thread_count - $r->threads_used >= $threads){
-		my $val = $r->thread_count - $r->threads_used ;
-print "value  $val \n";
+	if ($r->thread_count - $r->threads_used >= $threads && $r->is_live && $r->is_allocating){
+		$appserverid = $r->appserver_id;
+		last;
 		}
+	else { die "No suitable AppServer can be found. Exiting...\n"; }
 	}
+return $appserverid;
 }
 
+#assuming a valid AppInstance, Threadcount and AppServer have been defined, make a threadpack
 sub createTP{
-my ($ai, $tc) = shift;
+my ($ai, $tc, $as) = shift;
 my $tp = Threadpack->new(thread_count => $tc,
 			 appinstance_id => $ai,
+			 appserver_id => $ai,
+			 locked => '1',
 			);
 $tp->save;
 return $tp->threadpack_id;
 }
 
+#create a new AppInstance and associate it with the userid defined at the command line
 sub createAI{
 my ($uid) = shift;
 my $ai = Appinstance->new(account_id => $uid,
